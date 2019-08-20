ba3f92abad7f877f614c7d7319470f107cb66c2f
GenevieveBuckley <30920819+GenevieveBuckley@users.noreply.github.com>
2019-08-20 11:32:01 +1000

    Add ask_user function and tests

--- lamella/user_input.py
+++ lamella/user_input.py
@@ -4,6 +4,42 @@ import yaml
 from tkinter import Tk, filedialog
 
 
+def ask_user(message, default=None):
+    """Ask the user a question and return True if they say yes.
+
+    Parameters
+    ----------
+    message : str
+        The question to ask the user.
+    default : str, optional
+        If the user presses Enter without typing an answer,
+        the default indicates how to interpret this.
+        Choices are 'yes' or 'no'. The efault is None.
+
+    Returns
+    -------
+    bool
+        Returns True if the user answers yes, and false if they answer no.
+    """
+    yes = ['yes', 'y']
+    no = ['no', 'n']
+    if default:
+        if default.lower() == 'yes':
+            yes.append('')
+        elif default.lower() == 'no':
+            no.append('')
+    all_posiible_responses = yes + no
+    user_response = 'initial non-empty string'
+    while user_response not in all_posiible_responses:
+        user_response = input(message)
+        if user_response.lower() in yes:
+            return True
+        elif user_response.lower() in no:
+            return False
+        else:
+            print("Please enter 'yes' or 'no'")
+
+
 def choose_directory():
     """Ask the user to create or select an EMPTY directory with Tkinter.
 
--- tests/test_user_input.py
+++ tests/test_user_input.py
@@ -1,10 +1,29 @@
+from io import StringIO
 import os
 
-import numpy as np
 import pytest
 
 import lamella.user_input
-from lamella.validate import _validate_ion_beam_currents
+
+
+@pytest.mark.parametrize("test_input, default, expected", [
+    (StringIO('\n'), 'yes', True),
+    (StringIO('\n'), 'no', False),
+    (StringIO('y\n'), None, True),
+    (StringIO('Y\n'), None, True),
+    (StringIO('yes\n'), None, True),
+    (StringIO('Yes\n'), None, True),
+    (StringIO('YES\n'), None, True),
+    (StringIO('n\n'), None, False),
+    (StringIO('N\n'), None, False),
+    (StringIO('no\n'), None, False),
+    (StringIO('No\n'), None, False),
+    (StringIO('NO\n'), None, False),
+])
+def test_ask_user(monkeypatch, test_input, default, expected):
+    monkeypatch.setattr('sys.stdin', test_input)
+    result = lamella.user_input.ask_user('message', default=default)
+    assert result == expected
 
 
 @pytest.fixture
