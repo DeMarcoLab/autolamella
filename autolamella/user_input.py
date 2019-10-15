6603acd6fd5225428523984f09500ba11343993d
Genevieve Buckley <30920819+GenevieveBuckley@users.noreply.github.com>
2019-10-15 17:56:01 +1100

    Bugfix - only convert tilt angles to radians at the point where you need it

--- autolamella/user_input.py
+++ autolamella/user_input.py
@@ -89,8 +89,5 @@ def protocol_stage_settings(settings):
     for stage_settings in settings["lamella"]["protocol_stages"]:
         tmp_settings = settings["lamella"].copy()
         tmp_settings.update(stage_settings)
-        # Autoscript actually expects tilt in radians
-        radians_tilt = np.deg2rad(tmp_settings["overtilt_degrees"])
-        tmp_settings["overtilt_degrees"] = radians_tilt
         protocol_stages.append(tmp_settings)
     return protocol_stages
