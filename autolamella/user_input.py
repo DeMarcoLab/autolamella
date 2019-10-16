f67f6b609519ff8d1e14e32c8da68c7897e5e543
Genevieve Buckley <30920819+GenevieveBuckley@users.noreply.github.com>
2019-10-16 20:12:50 +1100

    Revert to try except for dictionary key errors

--- autolamella/user_input.py
+++ autolamella/user_input.py
@@ -14,8 +14,14 @@ def _add_missing_keys(dictionary):
     dictionary
         Python dictionray of user input settings.
     """
-    dictionary["lamella"].get("overtilt_degrees", 0)
-    dictionary.get("demo_mode", False)
+    try:
+        dictionary["lamella"]["overtilt_degrees"]
+    except KeyError:
+        dictionary["lamella"]["overtilt_degrees"] = 0
+    try:
+        dictionary["demo_mode"]
+    except KeyError:
+        dictionary["demo_mode"] = False
     return dictionary
 
 
