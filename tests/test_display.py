22b6dc4ebf0a656fb918d9d2e2b2ccc989a27ebc
GenevieveBuckley <30920819+GenevieveBuckley@users.noreply.github.com>
2019-12-10 11:14:12 +1100

    Increase tolerance, it seems Appveyor reports higher differences even when no problem

--- tests/test_display.py
+++ tests/test_display.py
@@ -5,7 +5,7 @@ import autolamella.data
 import autolamella.display
 
 
-@pytest.mark.mpl_image_compare
+@pytest.mark.mpl_image_compare(tolerance=3.1)
 def test_quick_plot():
     image = autolamella.data.mock_adorned_image()
     fig, ax = autolamella.display.quick_plot(image)
