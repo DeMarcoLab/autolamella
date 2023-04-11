d7fdd92b4fca304cc8122375fd0d3c31ff22109c
Patrick Cleeve <patrick.cleeve@monash.edu>
2023-04-11 15:31:36 +1000

    PPP

--- autolamella/utils.py
+++ autolamella/utils.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import os
 import fibsem.config as config
 
+# PPP: remove this and use the fibsem one with a default path in experiment
 def make_logging_directory(path: Path = None, name="run"):
     if path is None:
         path = os.path.join(config.BASE_PATH, "log")
