99037139563e6e8f625d6cdfe570934a9bf30c77
Patrick Cleeve <patrickcleeve2@gmail.com>
2024-03-01 13:07:24 +1100

    fix; typo

--- autolamella/workflows/core.py
+++ autolamella/workflows/core.py
@@ -175,7 +175,7 @@ def mill_undercut(
     # mill under cut
     lamella.protocol["undercut"] = deepcopy(settings.protocol["milling"]["undercut"])
     N_UNDERCUTS = int(settings.protocol["options"].get("undercut_tilt_angle_steps", 1))
-    UNDERCUT_ANGLE_DEG = settings.protocol["options"].get("under_tilt_tilt_angle", -5)
+    UNDERCUT_ANGLE_DEG = settings.protocol["options"].get("undercut_tilt_angle", -5)
     _UNDERCUT_V_OFFSET = lamella.protocol["undercut"].get("v_offset", 0e-6)
     undercut_stages = []
 
